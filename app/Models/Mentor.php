c865bfe624090872b26c58bb0dc2f50a737d2099
benbasse <moussabasse221@gmail.com>
2023-12-08 15:20:21 +0000

    tous les fonctionnalités sont la mais reste les ameliorations et les validations

--- app/Models/Mentor.php
+++ app/Models/Mentor.php
@@ -3,13 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class Mentor extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, HasApiTokens;
 
     public function Session()
     {
